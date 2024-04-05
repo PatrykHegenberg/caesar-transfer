b20bade6b7f8b8a4ca4454abbf19fb6d17c666bd
Patryk Hegenberg <patrykhegenberg@gmail.com>
2024-04-05 16:42:33 +0200

    added monorepo structure for ceasar-traffic and basic cli parsing with clap for test purpose

--- caesar_cli/src/main.rs
+++ caesar_cli/src/main.rs
@@ -1,3 +1,20 @@
+use clap::{Parser, Subcommand};
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    #[command(subcommand)]
+    cmd: Commands,
+}
+
+#[derive(Subcommand, Debug, Clone)]
+enum Commands {
+    Encrypt,
+    Decrypt,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+
+    println!("{:?}", args);
 }
