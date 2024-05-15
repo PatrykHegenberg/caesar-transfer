1f355a5d3e12b55b58a103b387021ce0ce4a82db
Manuel <manuel.keidel@entiac.com>
2024-05-15 10:40:15 +0200

    refactor lib.rs to work

--- caesar-tauri-angular/src-tauri/src/lib.rs
+++ caesar-tauri-angular/src-tauri/src/lib.rs
@@ -28,7 +28,7 @@ pub fn run() {
     env_logger::init();
     tauri::Builder::default()
         .plugin(tauri_plugin_shell::init())
-        .invoke_handler(tauri::generate_handler![receive, send, serve])
+        .invoke_handler(tauri::generate_handler![send, serve])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
